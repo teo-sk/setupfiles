f9b125fc7fb65a30f285c9964621c8ea67352858
Ondrej Slinták <ondrowan@gmail.com>
2013-01-22 16:50:52 +0100

    Fixed typo in pip condition

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ fi
 sudo apt-get install git vim zsh pidgin ack curl mc python-setuptools apache2 mysql-server postgresql php5 php5-cli php-pear php5-curl php5-gd php5-imagick php5-intl php5-mongo php5-mysqlnd php5-pgsql php5-sqlite php5-suhosin php5-xdebug
 
 # pip & virtualenv
-if command -v pip > /dev/null 2>&1; then
+if ! command -v pip > /dev/null 2>&1; then
     echo "\033[0;34mInstalling pip...\033[0m"
     sudo easy_install pip
 fi
@@ -52,3 +52,4 @@ git clone git://github.com/ondrowan/dotfiles.git $DOTFILES
 
 echo "\033[0;34mSymlinking .zshrc...\033[0m"
 ln -s $DOTFILES/.zshrc ~/.zshrc
+
